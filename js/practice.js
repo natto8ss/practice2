f4327f6c34eff2359b502fb7d5b5146ea995aa23
natto8ss <haruki.eri@gmail.com>
2021-11-13 01:01:22 +0900

    new

--- js/practice.js
+++ js/practice.js
@@ -1,40 +1,46 @@
-$(function() {
+//$(function() {
     //データ
-    var data = [
-        {
-            "id": "1",
-            "name": "青木優位",
-            "email": "sthompson0@spotify.com",
-            "gender": "男性"
-        },
-        {
-            "id": "2",
-            "name": "霧島隆",
-            "email": "kirisima@usgs.gov",
-            "gender": "男性"
-        },
-        {
-            "id": "3",
-            "name": "中島智子",
-            "email": "nakajima@usgs.gov",
-            "gender": "女性"
-        },
-        {
-            "id": "4",
-            "name": "鈴木洋子",
-            "email": "suzuki@usgs.gov",
-            "gender": "女性"
-        },
-        {
-            "id": "5",
-            "name": "佐藤隆",
-            "email": "satou@usgs.gov",
-            "gender": "男性"
-        }
-    ];
  
     //出力する処理
-    for(var i in data){
-        $("#contents_list").append("<tr><td>" + data[i].id + "</td>" + "<td>" + data[i].name + "</td>" + "<td>" + data[i].email + "</td>" + "<td>" + data[i].gender + "</td></tr>");
+    //for(var i in data){
+        //$("#contents_list").append("<tr><td>" + data[i].id + "</td>" + "<td>" + data[i].name + "</td>" + "<td>" + data[i].email + "</td>" + "<td>" + data[i].gender + "</td></tr>");
+    //}
+//});
+
+var contents = document.getElementById('contents_list');
+var data = [
+    {
+        "id": "1",
+        "name": "青木優位",
+        "email": "sthompson0@spotify.com",
+        "gender": "男性"
+    },
+    {
+        "id": "2",
+        "name": "霧島隆",
+        "email": "kirisima@usgs.gov",
+        "gender": "男性"
+    },
+    {
+        "id": "3",
+        "name": "中島智子",
+        "email": "nakajima@usgs.gov",
+        "gender": "女性"
+    },
+    {
+        "id": "4",
+        "name": "鈴木洋子",
+        "email": "suzuki@usgs.gov",
+        "gender": "女性"
+    },
+    {
+        "id": "5",
+        "name": "佐藤隆",
+        "email": "satou@usgs.gov",
+        "gender": "男性"
     }
-});
+];
+console.log(contents)
+for(var i in data){
+    contents.insertAdjacentHTML('beforeend', "<tr><td>" + data[i].id + "</td>" + "<td>" + data[i].name + "</td>" + "<td>" + data[i].email + "</td>" + "<td>" + data[i].gender + "</td></tr>");
+}
